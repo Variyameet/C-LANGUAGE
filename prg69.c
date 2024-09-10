376ff738eff80fdbee814b0b2e12b6f5b85d6d5d
meet <meetvariya08@gmail.com>
2024-09-10 15:55:10 +0530

    pattern

---
+++ prg69.c
@@ -0,0 +1,24 @@
+/*  AAAAA
+    BBBBB
+    CCCCC
+    DDDDD
+    EEEEE
+*/
+
+#include<stdio.h>
+int main()
+{
+
+    int i,j;
+    for(i=65;i<=69;i++)
+    {
+        for(j=65;j<=69;j++)
+        {
+            printf("%c",i);
+        }
+        printf("\n");
+
+    }
+    return 0;
+
+}
