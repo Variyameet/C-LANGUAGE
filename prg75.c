13b7145b2f714fafa14172fa4c4a5843d9f7b87f
meet <meetvariya08@gmail.com>
2024-09-12 15:42:46 +0530

    pattern

---
+++ prg75.c
@@ -0,0 +1,140 @@
+
+#include<stdio.h>
+int main()
+{
+    int i,j,sp=10;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=sp;j++)
+
+            printf(" ");
+            for(j=5;j>=i;j--)
+            {
+                printf(" *");
+            }
+            sp++;
+
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+////////////////////////////////
+
+
+
+/*      *
+        **
+        ***
+        ****
+        *****
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+            for(j=1;j<=i;j++)
+            {
+                printf("*");
+            }
+
+        printf("\n");
+    }
+    return 0;
+}
+
+
+//////////////////////////////////
+
+
+/*   ------*
+     ------**
+     ------***
+     ------****
+     ------*****
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,sp=10;
+    for(i=1;i<=5;i++)
+    {
+         for(j=1;j<=sp;j++)
+            printf("-");
+            for(j=1;j<=i;j++)
+            {
+                printf(" *");
+            }
+            printf("\n");
+           
+
+    }
+    return 0;
+}
+
+
+////////////////////////////////////
+
+/*-----*
+  ----**
+  ---***
+  --****
+  -*****
+
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j,sp=10;
+    for(i=1;i<=5;i++)
+    {
+         for(j=1;j<=sp;j++)
+            printf("-");
+            for(j=1;j<=i;j++)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+            sp--;
+    }
+    return 0;
+}
+
+
+
+
+
+//////////////////////////////////
+
+/*          *
+          *   *
+        *   *   *
+      *  *   *    *
+
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,sp=10;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=sp;j++)
+
+            printf(" ");
+            for(j=1;j<=i;j++)
+            {
+                printf(" *");
+            }
+            sp--;
+
+        printf("\n");
+    }
+    return 0;
+}
